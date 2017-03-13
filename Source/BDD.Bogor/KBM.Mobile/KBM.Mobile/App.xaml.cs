c489919afa41f70d0770a542d8521b8e57ae13a4
mifmasterz <mifmasterz@yahoo.com>
2017-03-14 05:04:05 +0700

    fixing android bug..

--- Source/BDD.Bogor/KBM.Mobile/KBM.Mobile/App.xaml.cs
+++ Source/BDD.Bogor/KBM.Mobile/KBM.Mobile/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using KBM.Mobile.Helper;
 using KBM.Mobile.Models;
 using KBM.Mobile.Views;
-using Plugin.Connectivity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,14 +26,7 @@ namespace KBM.Mobile
             DependencyService.Register<RestApi<UserProfile>>();
 
             SetMainPage();
-            if (!CrossConnectivity.Current.IsConnected)
-            {
-                Current.MainPage.DisplayAlert("Warning", "Hape antum tidak terkoneksi dengan jaringan internet", "OK");
-            }
-            CrossConnectivity.Current.ConnectivityChanged += (sender, args) =>
-            {
-                Current.MainPage.DisplayAlert("Connectivity Changed", "IsConnected: " + args.IsConnected.ToString(), "OK");
-            };
+         
         }
 
         public static void SetMainPage()
